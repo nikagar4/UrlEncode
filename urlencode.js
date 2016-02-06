b59728e27c4c210dab1a1916b8717d5f820116f1
nikagar4 <nikagaribashvili@yahoo.com>
2016-02-06 20:39:47 +0400

    Update urlencode.js
    Cleared Comments

--- urlencode.js
+++ urlencode.js
@@ -6,9 +6,8 @@ function urlEncode(str){
 		var result = []; 
 		for(var i = 0; i<str.length; i++){
 			var charCode = str.charCodeAt(i)
-			result.push(/*Number*/(hexChars.charAt(charCode >> 4)));
-			result.push(/*Number*/(hexChars.charAt(charCode & 15)));
-			// aba es Numberebi caushale da cesit unda gamovides
+			result.push((hexChars.charAt(charCode >> 4)));
+			result.push((hexChars.charAt(charCode & 15)));
 		}
 		
 		for(var i = 0; i<result.length; i+=2){
@@ -22,15 +21,14 @@ function urlEncode(str){
 		var result = []; 
 		for(var i = 0; i<str.length; i++){
 			var charCode = str.charCodeAt(i)
-			result.push(/*Number*/(hexChars.charAt(charCode >> 4)));
-			result.push(/*Number*/(hexChars.charAt(charCode & 15))); 
+			result.push((hexChars.charAt(charCode >> 4)));
+			result.push((hexChars.charAt(charCode & 15))); 
 		}
 		
 		for(var i = 0; i<result.length; i+=2){
 			encodedResult += '%' + result[i] + result[i+1];
 		}
 		return encodedResult;
-		// wertil-mdzime gaklda mashin ro ver vaketebdit :))
 	}else if(typeof str === 'object' && util.isArray(str)){
 		str = str.join(',');
 		var encodedResult = '';
@@ -39,8 +37,8 @@ function urlEncode(str){
 		for(var i = 0; i<str.length; i++){
 			if(str[i] == ','){result.push(str[i]); continue;}
 			var charCode = str.charCodeAt(i)
-			result.push(/*Number*/(hexChars.charAt(charCode >> 4)));
-			result.push(/*Number*/(hexChars.charAt(charCode & 15))); 
+			result.push((hexChars.charAt(charCode >> 4)));
+			result.push((hexChars.charAt(charCode & 15))); 
 		}
 		var j = 0;
 		encodedResult += j++ + '=';
@@ -56,14 +54,12 @@ function urlEncode(str){
 				var and = '&';
 			}
 			if(i+2==result.length){
-				// es boloshic roar daamatos & magito mivacere da tu uketess moifiqreb ichaliche ise mushaobs
 				var and = '';
 			}
 			encodedResult += '%' + result[i] + result[++i] + and;
 		}
 		return encodedResult;
 	}else if(typeof str === 'object'){
-		//objectzec daaxloebit rogorc arrayshi gavakete egre gaakete
 		var encodedResult = '';
 		var hexChars = "0123456789ABCDEF";
 		var result = []; 
@@ -71,14 +67,14 @@ function urlEncode(str){
 			var val = str[key];
 			for(var i = 0; i<key.length; i++){
 				var charCode = key.charCodeAt(i)
-				result.push(/*Number*/(hexChars.charAt(charCode >> 4)));
-				result.push(/*Number*/(hexChars.charAt(charCode & 15))); 
+				result.push((hexChars.charAt(charCode >> 4)));
+				result.push((hexChars.charAt(charCode & 15))); 
 			}
 			result.push('=');
 			for(var i = 0; i<val.length; i++){
 				var charCode = val.charCodeAt(i)
-				result.push(/*Number*/(hexChars.charAt(charCode >> 4)));
-				result.push(/*Number*/(hexChars.charAt(charCode & 15)));
+				result.push((hexChars.charAt(charCode >> 4)));
+				result.push((hexChars.charAt(charCode & 15)));
 			}
 			result.push('&');
 		}
@@ -96,8 +92,4 @@ function urlEncode(str){
 		return encodedResult;
 	}
 }
-// karoche garib gadaxede amas da mgoni yvelaferi mushaobs ukve tu rames dainaxav chaamate da mitxari mec
-
-
-
 // made by nika and shako
